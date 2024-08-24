730bb41f9aae031db644e86662f67bf390596772
Artem Romanenko <ar5336@gmail.com>
2024-08-23 21:45:13 -0700

    testing

--- test.cpp
+++ test.cpp
@@ -27,79 +27,146 @@ int total = 0;
 int passed = 0;
 int failed = 0;
 
-string test_utterance = "";
+ParserTester tester = ParserTester();
 
-Parser test_parser;
-
-Grammar test_grammar = Grammar();
-
-// Displayer displayer = Displayer("reader");
-
-PredicateTemplateHandler test_predicate_template_handler = PredicateTemplateHandler();
-
-PredicateHandler test_predicate_handler = PredicateHandler(&test_predicate_template_handler);
-
-ConceptualSchema test_conceptual_schema = ConceptualSchema();
-
-Mind test_mind = Mind(&test_predicate_handler, &test_conceptual_schema);
-
-void setup_parse()
+ParserTester::ParserTester()
+    : test_predicate_handler(PredicateHandler(nullptr)),
+        test_mind(Mind(nullptr, nullptr))
 {
+    test_utterance = "";
+
+    test_parser = Parser();
+
+    test_grammar = Grammar();
+
+    // Displayer displayer = Displayer("reader");
+
+    test_predicate_template_handler = PredicateTemplateHandler();
+
+    test_predicate_handler = PredicateHandler(&test_predicate_template_handler);
+
+    test_conceptual_schema = ConceptualSchema();
+
+    test_mind = Mind(&test_predicate_handler, &test_conceptual_schema);
+}
+
+void test_handler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
+
+void ParserTester::setup_parse()
+{
+    if(DEBUGGING)
+        printf("setting up parse for test\n");
+    test_predicate_template_handler = PredicateTemplateHandler();
+
+    test_grammar = Grammar();
+
+	signal(11, test_handler);   // install our handler
+
+
     GrammarReader test_reader = GrammarReader(&test_grammar, &test_predicate_handler, &test_predicate_template_handler);
 	test_reader.read_grammar("grammar.txt");
 
-	// translate the read frames into cnf frames
+ 	// translate the read frames into cnf frames
 	test_grammar.binarize_grammar();
 
+    test_predicate_handler = PredicateHandler(&test_predicate_template_handler);
+
+    test_conceptual_schema = ConceptualSchema();
+
 	test_parser = Parser(test_grammar);
-
 	test_predicate_handler.predicate_template_handler = &test_predicate_template_handler;
-
 	test_parser.predicate_handler = &test_predicate_handler;
 
+
 	test_predicate_handler.init_stringification();
+
+    test_mind = Mind(&test_predicate_handler, &test_conceptual_schema);
+
 }
 
-void run_parse(string utterance)
+void ParserTester::run_parse(string utterance)
 {
 	test_parser.update_parse_grid(utterance);
 
     Frame frame = Frame();
-    if (test_parser.try_get_top_interpretation(frame));
-
-    Expression constructed_expression = frame.accumulated_expression;
-
-    test_mind.tell(constructed_expression);
-
+    if (test_parser.try_get_top_interpretation(frame))
+    {
+        if (DEBUGGING)
+        {
+            printf("recieved expression from parser\n");
+        }
+        Expression constructed_expression = frame.accumulated_expression;
+        test_mind.tell(constructed_expression);
+    }
 }
 
 bool test_parse__event() {
-    setup_parse();
-	run_parse("a dog bit a man");
+    tester.setup_parse();
+	tester.run_parse("a dog bit a man");
 
-    TEST_ASSERT(test_mind.concrete_nouns.size() == 2);
-    TEST_ASSERT(equals(test_mind.concrete_nouns.at(0).entity_type->noun, "dog"));
-    TEST_ASSERT(equals(test_mind.concrete_nouns.at(1).entity_type->noun, "man"));
+    TEST_ASSERT(tester.test_mind.concrete_nouns.size() == 2);
+    TEST_ASSERT(equals(tester.test_mind.concrete_nouns.at(0).entity_type->noun, "dog"));
+    TEST_ASSERT(equals(tester.test_mind.concrete_nouns.at(1).entity_type->noun, "man"));
 
     return true;
 }
 
 bool test_parse__anaphora_event() {
-    setup_parse();
-	run_parse("a dog bit a man");
-	run_parse("the dog that bit a man is ugly");
+    tester.setup_parse();
+	tester.run_parse("a dog bit a man");
+	tester.run_parse("the dog that bit a man is ugly");
 
-    TEST_ASSERT(test_mind.concrete_nouns.size() == 2);
-    TEST_ASSERT(equals(test_mind.concrete_nouns.at(0).entity_type->noun, "dog"));
-    TEST_ASSERT(equals(test_mind.concrete_nouns.at(1).entity_type->noun, "man"));
+    TEST_ASSERT(tester.test_mind.concrete_nouns.size() == 2);
+    TEST_ASSERT(equals(tester.test_mind.concrete_nouns.at(0).entity_type->noun, "dog"));
+    TEST_ASSERT(equals(tester.test_mind.concrete_nouns.at(1).entity_type->noun, "man"));
 
-    TEST_ASSERT(equals(test_mind.concrete_nouns.at(1).entity_type->noun, "man"));
+    TEST_ASSERT(tester.test_mind.concrete_nouns.at(0).properties.count("ugly") == 1);
+
+    return true;
+}
+
+bool test_parse__event_anaphora_create_event() {
+    tester.setup_parse();
+	tester.run_parse("a dog bit a man");
+	tester.run_parse("the dog that bit a man is ugly");
+	tester.run_parse("the dog that bit a man ate some grass");
+
+    TEST_ASSERT(tester.test_mind.concrete_nouns.size() == 3);
+    TEST_ASSERT(equals(tester.test_mind.concrete_nouns.at(0).entity_type->noun, "dog"));
+    TEST_ASSERT(equals(tester.test_mind.concrete_nouns.at(1).entity_type->noun, "man"));
+    TEST_ASSERT(equals(tester.test_mind.concrete_nouns.at(2).entity_type->noun, "grass"));
+
+    TEST_ASSERT(tester.test_mind.timeline.actions.size() == 2);
+
+    Event dbm = tester.test_mind.timeline.actions.at(0);
+    TEST_ASSERT(equals(dbm.actor_noun_class, "dog"));
+    TEST_ASSERT(equals(dbm.subject_noun_class, "man"));
+
+    Event dag = tester.test_mind.timeline.actions.at(1);
+    TEST_ASSERT(equals(dag.actor_noun_class, "dog"));
+    TEST_ASSERT(equals(dag.subject_noun_class, "grass"));
+
+    TEST_ASSERT(tester.test_mind.concrete_nouns.at(0).properties.count("ugly") == 1);
 
     return true;
 }
 
 int test_all() {
     RUN_TEST(test_parse__event);
+    RUN_TEST(test_parse__anaphora_event);
+    RUN_TEST(test_parse__event_anaphora_create_event);
 
     std::cout << "\n\nTest Results:\n";
     std::cout << "Total tests: " << total << std::endl;
--- test.hpp
+++ test.hpp
@@ -1,12 +1,45 @@
 #ifndef TEST
 #define TEST
 
+#include <execinfo.h>
 #include <iostream>
+#include <signal.h>
 
 #include "grammar_reader.hpp"
 #include "interpretation.hpp"
 #include "mind.hpp"
 #include "global.hpp"
+
+class ParserTester
+{
+private:
+
+    string test_utterance;
+
+    Parser test_parser;
+
+    Grammar test_grammar;
+
+    // Displayer displayer = Displayer("reader");
+
+    PredicateTemplateHandler test_predicate_template_handler;
+
+    PredicateHandler test_predicate_handler;
+
+    ConceptualSchema test_conceptual_schema;
+
+public:
+
+    Mind test_mind;
+
+    void run_parse(string utterance);
+
+    void setup_parse();
+
+    ParserTester();
+
+
+};
 
 int test_all();
 
